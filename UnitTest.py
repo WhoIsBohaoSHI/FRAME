207b5ecde4a5105396e3507e98f310dba4adbe04
WhoIsBohaoSHI <bohao@zju.edu.cn>
2018-11-13 14:49:33 +0800

    ““unit test for convolution

--- UnitTest.py
+++ UnitTest.py
@@ -11,11 +11,14 @@ from Convolution import convolution
 
 class testFunction(unittest.TestCase):
     def testConvolution(self):
-        image = np.array([[1.0,0.0,1.0,0.0,1.0], [1.0,0.0,1.0,0.0,1.0], [1.0,0.0,1.0,0.0,1.0], [1.0,0.0,1.0,0.0,1.0], [1.0,0.0,1.0,0.0,1.0]])
+        image = np.array([[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0], [1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],
+                          [1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0],[1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0]])
         convolutionFilter = np.array([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4],[1,4,7,4,1]])/271.0
         filteredImage = convolution(convolutionFilter, image)
+        print (filteredImage)
         
-        self.assertAlmostEquals(filteredImage[0,0], 141.0/271.0)
+        self.assertAlmostEquals(filteredImage[2,2], 141.0/271.0)
+        self.assertAlmostEquals(filteredImage[3,3], 132.0/271.0)
     
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(testFunction)
